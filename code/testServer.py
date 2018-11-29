7333ca1eb43704f7fb63a8fe4f6488e411c7a7ca
Pierre Nicolay <p.nicolay@student.uliege.be>
2018-11-29 15:06:43 +0100

    Srver running in a thread

--- code/testServer.py
+++ code/testServer.py
@@ -1,12 +1,42 @@
 from flask import Flask, request
 import http.client as httplib
+from threading import Thread
+import time
 
 app = Flask(__name__)
 
-@app.route('/connectedUsers')
-def index():
-    return 'Hello world'
+
+class simpletP2P(object):
+    def __init__(self, host, port):
+        server = Thread(target = launchServer, args = (host, port))
+        server.start()
+        time.sleep(2)
+        conn = httplib.HTTPConnection("{}:{}".format(host, port))
+        conn.request("GET","/connectedUsers")
+        res = conn.getresponse()
+        print(res.read())
+        conn.request("GET","/niqueTaMereBen")
+        res = conn.getresponse()
+        print(res.read())
+
+    @app.route('/connectedUsers')
+    def index():
+        return 'Hello world'
+
+    def coucou():
+        return 'coucou'
+
+    @app.route('/niqueTaMereBen')
+    def YES():
+        return 'bien joue'
+
+
+def launchServer(host, port):
+    app.run(debug=True, use_reloader=False, host=host, port=port)
+
+
+def main():
+    sp = simpletP2P("0.0.0.0", "8080")
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
-
+    main()
