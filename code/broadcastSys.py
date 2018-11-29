b593f895476a98b8d97ce5fdb08119c62842b8cb
bendelv <ben.delvoye@gmail.com>
2018-11-29 15:18:18 +0100

    Peer Client Server class def

---
+++ code/broadcastSys.py
@@ -0,0 +1,15 @@
+from flask import Flask
+
+
+class Communication:
+    def __init__(self, bootstrapAdd):
+        "retrieve all connected adress nodes from bootstrap node"
+        return pass
+
+    def publish(self, jsonObj, users):
+        "Send a json object to all connected user"
+        return pass
+
+    def listen(self):
+        ""
+        return pass
