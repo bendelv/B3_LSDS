954843b7c1ef981dcf8815165ab147faa94689ec
bendelv <ben.delvoye@gmail.com>
2018-11-29 18:35:06 +0100

    test simple broadcast

--- code/peer.py
+++ code/peer.py
@@ -14,17 +14,28 @@ class Peer:
 
 class Server:
     def __init__(self, host, port):
-        self.app = Flask(__name__)
-        self.app.add_url_rule('/getBlockChain', 'coucou', self.coucou)
         server = Thread(target = self.launchServer, args = (host, port))
         server.start()
         pass
 
     def launchServer(self, host, port):
+        self.app = Flask(__name__)
+        self.app.add_url_rule('/getBlockChain', 'coucou', self.sendBlockChain)
+        self.app.add_url_rule('/addPeer/<newPeer>', 'coucou', self.addPeer)
+        self.app.add_url_rule('/rcvMsg/?msg=<msg>', 'coucou', self.rcvMsg, methods = 'POST')
+
         self.app.run(debug=True, use_reloader=False, host=host, port=port)
 
-    def coucou(self):
+    def sendBlockChain(self):
         return "coucou"
+
+    def addPeer(self):
+        pass
+
+    def rcvMsg(self):
+        msg = request.args.get('msg', '')
+        print(msg)
+        pass
 
 class Client:
     def __init__(self):
@@ -43,7 +54,7 @@ class Client:
         for connected in self.connected:
             headers = {'Content-type': 'application/json'}
             conn = httplib.HTTPConnection("{}".format(connected))
-            conn.request("POST","/post", jsonData, headers)
+            conn.request("POST", "/rcvMsg/?msg={}".format(jsonData), headers)
             res = conn.getresponse()
         pass
 
