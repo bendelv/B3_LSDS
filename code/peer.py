adf1e10f4ef3463a745bfec96936f799c4d188a5
bendelv <ben.delvoye@gmail.com>
2018-11-29 19:35:04 +0100

    test broadcast

--- code/peer.py
+++ code/peer.py
@@ -5,11 +5,9 @@ import json
 import time
 
 class Peer:
-    def __init__(self, host, port):
-        self.host = host
-        self.port = port
-        self.serverSide = Server(host, port)
-        self.clientSide = Client()
+    def __init__(self, bootsDist, bootLoc):
+        self.serverSide = Server(bootsLoc[0], bootsLoc[1])
+        self.clientSide = Client(bootsDist[0], bootsDist[1])
         pass
 
 class Server:
@@ -38,8 +36,8 @@ class Server:
         return "msg received"
 
 class Client:
-    def __init__(self):
-        self.connected = ['192.168.1.25:5000']
+    def __init__(self, host, port):
+        self.connected = []
         pass
 
     def getBlockChain(self):
@@ -49,15 +47,18 @@ class Client:
         print(res.read())
 
     def broadcast(self, msg):
-
         for connected in self.connected:
             conn = httplib.HTTPConnection("{}".format(connected))
             conn.request("POST", "/rcvMsg/?msg={}".format(msg))
             res = conn.getresponse()
-        pass
+
+    def fetchConnected(self):
+        conn = httplib.HTTPConnection("{}:{}".format(self.host, self.port))
+        conn.request("GET","/getBlockChain")
+        res = conn.getresponse()
 
 def main():
-    peer = Peer('localhost', 5000)
+    peer = Peer('192.168.1.60', 5000)
     time.sleep(2)
     peer.clientSide.broadcast("Coucou")
     return 0
